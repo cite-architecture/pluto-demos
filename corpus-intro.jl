499e667e796d6e84ecdc5187db82d7f2b44adb57
Neel Smith <nsmith@Technites.local>
2020-12-31 07:31:05 -0500

    Illustrate a possible implementation of basic navigation

--- corpus-intro.jl
+++ corpus-intro.jl
@@ -5,6 +5,8 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 2c2cc564-4b59-11eb-1c05-bd9711a4a255
+# Idiom to keep definition of julia environment self-contained 
+# in a replicable Pluto notebook.
 begin
 	using Pkg
 	Pkg.add(url="https://github.com/cite-architecture/CitableText.jl")
@@ -13,10 +15,7 @@ begin
 end
 
 # ╔═╡ 6c780750-4b59-11eb-1802-a9789ae3b447
-using CitableText
-
-# ╔═╡ d5557334-4b59-11eb-20c1-7dfae2006a8a
-using CSV, HTTP
+using CitableText, CSV, HTTP
 
 # ╔═╡ 1b8c61f6-4b59-11eb-18fd-8fcdfdddc637
 md"# Corpus experiments
@@ -25,7 +24,9 @@ md"# Corpus experiments
 "
 
 # ╔═╡ 8897c3d8-4b60-11eb-08bf-7bb72571eaa9
-md"This is the type we want to add to the package.
+md"## Type for citable corpus 
+
+This defines the new type we want to add to the package.
 "
 
 # ╔═╡ faba8762-4b5f-11eb-1ed3-5d844bc5159e
@@ -34,7 +35,9 @@ struct Corpus
 end
 
 # ╔═╡ 994facd6-4b60-11eb-12c2-c91b7a6951f2
-md"Let's build one from a CEX file using Julia's `CSV` module."
+md"## Build from URL
+
+Let's build one from a CEX file using Julia's `CSV` module."
 
 # ╔═╡ f9b08e70-4b5a-11eb-25e4-c5a2928de42c
 url = "https://raw.githubusercontent.com/LinguaLatina/texts/master/texts/latin23/hyginus.cex"
@@ -64,22 +67,56 @@ typeof(corpusdata)
 c = Corpus(corpusdata)
 
 # ╔═╡ e0761370-4b60-11eb-059e-0f7ee030193e
-md"Slice and dice, filter, whatever you like."
+md"## Use the corpus
+
+Slice and dice, filter, whatever you like."
 
 # ╔═╡ 0f92ccbc-4b60-11eb-2841-2dba4816906f
 c.corpus[100:120]
 
 # ╔═╡ 4fa371da-4b60-11eb-237a-0fd324cd1a2f
-argonauts = CtsUrn("urn:cts:latinLit:stoa1263.stoa001.hc:14pr")
+argonautsUrn = CtsUrn("urn:cts:latinLit:stoa1263.stoa001.hc:14pr")
 
 # ╔═╡ 5388caa2-4b60-11eb-0cd3-59a9ca43cf12
-filter(cn -> urncontains(argonauts,cn.urn), c.corpus)
+argonauts = filter(cn -> urncontains(argonautsUrn,cn.urn), c.corpus)
+
+# ╔═╡ 8919b148-4b62-11eb-29dc-bfcb23544f74
+n1 = argonauts[1]
+
+# ╔═╡ ab409d04-4b62-11eb-24bd-dd70aed72491
+findfirst(isequal(n1), c.corpus)
+
+# ╔═╡ d971a2e0-4b62-11eb-30e0-67cd81f88d99
+c.corpus[98]
+
+# ╔═╡ e8273c62-4b62-11eb-2d4c-13600de238f1
+function indexof(n::CitableNode, corp::Corpus)
+	findfirst(isequal(n), corp.corpus)
+end
+
+# ╔═╡ 58067cc0-4b63-11eb-040c-4d1d3feff520
+function nextnode(n::CitableNode, corp::Corpus)
+	idx = indexof(n,corp)
+	if  idx == size(corp.corpus)
+		throw(ArgumentError("Last node already."))
+	else
+		corp.corpus[idx + 1]
+	end
+end
+
+# ╔═╡ 2b1d7cfe-4b63-11eb-2cbe-bd5aa27b2460
+indexof(argonauts[4], c)
+
+# ╔═╡ c622a79c-4b63-11eb-11ea-bd03e795bd36
+argotitle = argonauts[1]
+
+# ╔═╡ d7ec34fc-4b63-11eb-3e3e-dd4721536f98
+nextnode(argotitle, c)
 
 # ╔═╡ Cell order:
 # ╟─1b8c61f6-4b59-11eb-18fd-8fcdfdddc637
 # ╠═2c2cc564-4b59-11eb-1c05-bd9711a4a255
 # ╠═6c780750-4b59-11eb-1802-a9789ae3b447
-# ╠═d5557334-4b59-11eb-20c1-7dfae2006a8a
 # ╟─8897c3d8-4b60-11eb-08bf-7bb72571eaa9
 # ╠═faba8762-4b5f-11eb-1ed3-5d844bc5159e
 # ╟─994facd6-4b60-11eb-12c2-c91b7a6951f2
@@ -96,3 +133,11 @@ filter(cn -> urncontains(argonauts,cn.urn), c.corpus)
 # ╠═0f92ccbc-4b60-11eb-2841-2dba4816906f
 # ╠═4fa371da-4b60-11eb-237a-0fd324cd1a2f
 # ╠═5388caa2-4b60-11eb-0cd3-59a9ca43cf12
+# ╠═8919b148-4b62-11eb-29dc-bfcb23544f74
+# ╠═ab409d04-4b62-11eb-24bd-dd70aed72491
+# ╠═d971a2e0-4b62-11eb-30e0-67cd81f88d99
+# ╠═e8273c62-4b62-11eb-2d4c-13600de238f1
+# ╠═58067cc0-4b63-11eb-040c-4d1d3feff520
+# ╠═2b1d7cfe-4b63-11eb-2cbe-bd5aa27b2460
+# ╠═c622a79c-4b63-11eb-11ea-bd03e795bd36
+# ╠═d7ec34fc-4b63-11eb-3e3e-dd4721536f98
