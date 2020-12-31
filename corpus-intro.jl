7f3bb8d0391127d7bfed89ae71dc1611550d9b39
Neel Smith <dnsmith.neel@gmail.com>
2020-12-31 09:58:07 -0500

    Update corpus notebook

--- corpus-intro.jl
+++ corpus-intro.jl
@@ -21,35 +21,27 @@ using CSV, HTTP
 # ╔═╡ 1b8c61f6-4b59-11eb-18fd-8fcdfdddc637
 md"# Corpus experiments
 
+
+## Using the `CitableText` module
+
 `CitableText` has been submitted to the Julia package registry.  While waiting for it to be registered, you can use it directly from its github repository.
 "
 
-# ╔═╡ 8897c3d8-4b60-11eb-08bf-7bb72571eaa9
-md"This is the type we want to add to the package.
-"
-
-# ╔═╡ faba8762-4b5f-11eb-1ed3-5d844bc5159e
-struct Corpus
-    corpus::Array{CitableNode, 1}
-end
-
 # ╔═╡ 994facd6-4b60-11eb-12c2-c91b7a6951f2
-md"Let's build one from a CEX file using Julia's `CSV` module."
+md"## Manually build a corpus
+
+Let's manually build one from a CEX file using Julia's `CSV` module.
+
+"
 
 # ╔═╡ f9b08e70-4b5a-11eb-25e4-c5a2928de42c
 url = "https://raw.githubusercontent.com/LinguaLatina/texts/master/texts/latin23/hyginus.cex"
 
 # ╔═╡ eeb3ffa0-4b5c-11eb-1cb5-45d8111a38cf
-raw = HTTP.get(url).body
-
-# ╔═╡ 00ae276c-4b5d-11eb-3173-bf284e87406a
-typeof(raw)
+rawread = HTTP.get(url).body
 
 # ╔═╡ f8bcd34e-4b59-11eb-380f-e1f0f1107e0c
-f = CSV.File(raw, skipto=2, delim="#")  |> Array
-
-# ╔═╡ 4c893f34-4b5b-11eb-2047-c5ebb2061a59
-typeof(f)
+f = CSV.File(rawread, skipto=2, delim="#")  |> Array
 
 # ╔═╡ c79173f4-4b60-11eb-30e5-c972da4d4329
 md"Map array of CSV rows to an array of `CitableNode`s, and we're as good as done."
@@ -57,14 +49,13 @@ md"Map array of CSV rows to an array of `CitableNode`s, and we're as good as don
 # ╔═╡ 4d5e271e-4b5b-11eb-1a2c-298e2bcf6a7a
 corpusdata = map(row -> (CitableNode(CtsUrn(row[1]), row[2])),  f)
 
-# ╔═╡ cdea1172-4b5e-11eb-22a7-97bc56cdc6d0
-typeof(corpusdata)
-
 # ╔═╡ fd3e82f4-4b5f-11eb-124c-579720e348d1
-c = Corpus(corpusdata)
+c = CitableCorpus(corpusdata)
 
 # ╔═╡ e0761370-4b60-11eb-059e-0f7ee030193e
-md"Slice and dice, filter, whatever you like."
+md"## Working with the array of citable nodes
+
+Slice and dice, filter, whatever you like."
 
 # ╔═╡ 0f92ccbc-4b60-11eb-2841-2dba4816906f
 c.corpus[100:120]
@@ -80,17 +71,12 @@ filter(cn -> urncontains(argonauts,cn.urn), c.corpus)
 # ╠═2c2cc564-4b59-11eb-1c05-bd9711a4a255
 # ╠═6c780750-4b59-11eb-1802-a9789ae3b447
 # ╠═d5557334-4b59-11eb-20c1-7dfae2006a8a
-# ╟─8897c3d8-4b60-11eb-08bf-7bb72571eaa9
-# ╠═faba8762-4b5f-11eb-1ed3-5d844bc5159e
 # ╟─994facd6-4b60-11eb-12c2-c91b7a6951f2
 # ╟─f9b08e70-4b5a-11eb-25e4-c5a2928de42c
 # ╠═eeb3ffa0-4b5c-11eb-1cb5-45d8111a38cf
-# ╠═00ae276c-4b5d-11eb-3173-bf284e87406a
 # ╠═f8bcd34e-4b59-11eb-380f-e1f0f1107e0c
-# ╠═4c893f34-4b5b-11eb-2047-c5ebb2061a59
 # ╟─c79173f4-4b60-11eb-30e5-c972da4d4329
 # ╠═4d5e271e-4b5b-11eb-1a2c-298e2bcf6a7a
-# ╠═cdea1172-4b5e-11eb-22a7-97bc56cdc6d0
 # ╠═fd3e82f4-4b5f-11eb-124c-579720e348d1
 # ╟─e0761370-4b60-11eb-059e-0f7ee030193e
 # ╠═0f92ccbc-4b60-11eb-2841-2dba4816906f
